c14fe890ae1eea4e0fc529e3f6362a744c9efa86
carjam120443-netizen <carjam120443@gmail.com>
2026-08-26 17:33:05 -0400

    Fix kernel package architecture handling

--- build/build.sh
+++ build/build.sh
@@ -14,8 +14,10 @@ fi
 
 lb clean --purge
 
-# Ubuntu Noble mirrors. Only use mirror options supported by the Debian
-# live-build version installed by the GitHub Actions workflow.
+# Ubuntu Noble mirrors. live-build appends the architecture to the value of
+# --linux-packages, so do not use linux-image-generic here: it would become
+# linux-image-generic-amd64. Instead, leave the kernel package unset and add
+# Ubuntu's real kernel metapackage as a normal chroot package.
 lb config \
     --distribution noble \
     --architectures amd64 \
@@ -25,9 +27,11 @@ lb config \
     --mirror-chroot-security http://security.ubuntu.com/ubuntu \
     --mirror-binary http://archive.ubuntu.com/ubuntu \
     --mirror-binary-security http://security.ubuntu.com/ubuntu \
-    --linux-packages "linux-image-generic" \
     --binary-images iso-hybrid \
     --debian-installer none
+
+mkdir -p config/package-lists
+printf '%s\n' 'linux-image-generic' > config/package-lists/winshift-kernel.list.chroot
 
 lb build
 
